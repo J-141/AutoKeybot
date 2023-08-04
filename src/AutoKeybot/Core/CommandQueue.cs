3173216a0c58adf04c9a41c4d07074f4fd17fec0
J-141 <jean.chen141@gmail.com>
2023-08-04 03:53:22 -0500

    fix insert

--- src/AutoKeybot/Core/CommandQueue.cs
+++ src/AutoKeybot/Core/CommandQueue.cs
@@ -8,20 +8,20 @@ namespace AutoKeybot.Core {
 
         public void Enqueue(IControllerCommand item) {
             lock (_lock) {
-                _queue.AddLast(item);
+                _queue.AddFirst(item);
             }
         }
 
         public void Insert(IControllerCommand item) {
             lock (_lock) {
-                _queue.AddFirst(item);
+                _queue.AddLast(item);
             }
         }
 
         public void InsertBatch(IEnumerable<IControllerCommand> items) {
             lock (_lock) {
                 foreach (var item in items.Reverse()) {
-                    _queue.AddFirst(item);
+                    _queue.AddLast(item);
                 }
             }
         }
@@ -29,7 +29,7 @@ namespace AutoKeybot.Core {
         public void EnqueueBatch(IEnumerable<IControllerCommand> items) {
             lock (_lock) {
                 foreach (var item in items) {
-                    _queue.AddLast(item);
+                    _queue.AddFirst(item);
                 }
             }
         }
